8c526e29091af71c4a42278f4ec852eb61e11b77
avanishmani <avanishmanitripathi@gmail.com>
2023-06-21 23:13:44 +0530

    Exception Handling Thing is Done

---
+++ HostBook/src/main/java/com/HostBook/Exception/EmployeeException.java
@@ -0,0 +1,12 @@
+package com.HostBook.Exception;
+
+public class EmployeeException extends Exception {
+	public EmployeeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
